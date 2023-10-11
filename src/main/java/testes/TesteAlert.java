3ac48efde3080cec7826570aa1ac1de467327779
GUILHERME DINIZ <gdiniztec@yahoo.com.br>
2023-10-10 22:44:25 -0300

    33. Alert: Confirm

--- src/main/java/testes/TesteAlert.java
+++ src/main/java/testes/TesteAlert.java
@@ -23,6 +23,32 @@ public class TesteAlert {
 		Assert.assertEquals("Alert Simples", texto);
 		alert.accept();
 		driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
-		
+
 	}
+
+	@Test
+
+	public void deveInteragirComAlertConfirm() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setPosition(new Point(1350, 10));
+		driver.manage().window().maximize();
+		driver.get("file:///C:/selenium/Curso/formulario/componentes.html");
+		driver.findElement(By.id("confirm")).click();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", alerta.getText());
+		alerta.accept();
+		Assert.assertEquals("Confirmado", alerta.getText());
+		alerta.accept();
+
+		driver.findElement(By.id("confirm")).click();
+		alerta = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", alerta.getText());
+		alerta.dismiss();
+		Assert.assertEquals("Negado", alerta.getText());
+		alerta.dismiss();
+
+		driver.quit();
+
+	}
+
 }
